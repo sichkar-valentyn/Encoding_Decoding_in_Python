a684cfce21d5c80684c2dab2e2adf10cd5b56fbe
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-02-10 17:16:34 +0300

    Uploading file
    Adding file into the repository

---
+++ encoding_decoding.py
@@ -0,0 +1,21 @@
+
+# Method encode() returns an encoded version of the string
+initial_string = 'Hello there!'
+encoded_string = initial_string.encode('utf-32')
+
+# To choose the right Codec for needed language go to the page:
+# docs.python.org/3.7/library/codecs.html
+# Or choose 'ut-32' as standard for all languages
+
+print('Encoded string of "', initial_string, '" is: ')
+print(encoded_string)
+
+# To decode string we need to use decode function
+decoded_string = b'\xff\xfe\x00\x00H\x00\x00\x00e\x00\x00\x00l\x00\x00\x00l\x00\x00\x00o\x00\x00\x00 \x00\x00\x00t\x00\x00\x00h\x00\x00\x00e\x00\x00\x00r\x00\x00\x00e\x00\x00\x00!\x00\x00\x00'.decode('utf-32')
+print(decoded_string)
+
+# It is possile to use "Fill paragraph" option to show long string in the screen
+decoded_string = b'\xff\xfe\x00\x00H\x00\x00\x00e\x00\x00\x00l\x00\x00\x00l\x00\x00\x00o\x00\x00\x00 ' \
+                 b'\x00\x00\x00t\x00\x00\x00h\x00\x00\x00e\x00\x00\x00r\x00\x00\x00e\x00\x00\x00!\x00\x00\x00' \
+                 b''.decode('utf-32')
+print(decoded_string)
